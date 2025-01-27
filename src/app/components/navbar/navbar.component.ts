c6b810e38cfe894992a5eb4a03c8bfaebf7cd057
mahmouddessoki <mahmouddessoki33@gmail.com>
2025-01-27 16:47:21 +0200

    third update

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink , RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit {
 
-  onScroll(e : Event): void {
-    console.log(e)
+  @ViewChild("navbar") navbar!: ElementRef;
+
+
+  OnScroll(): void {
+    document.addEventListener('scroll', (e) => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition >= 55) {
+        this.navbar.nativeElement.classList.remove('p-3')
+      } else {
+        this.navbar.nativeElement.classList.add('p-3')
+      }
+    })
   }
+  ngAfterViewInit(): void {
+    this.OnScroll()
 
+  }
+  ngAfterViewChecked(): void {
+    this.OnScroll()
+  }
 
 }
